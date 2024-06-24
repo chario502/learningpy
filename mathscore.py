3478ce23d9bafb42120880fd6ebdc05203ed2d75
chario502 <168148447+chario502@users.noreply.github.com>
2024-06-23 20:47:48 -0400

    Create mathscore.py
    tells you your letter grade based on your numerical grade.

---
+++ mathscore.py
@@ -0,0 +1,12 @@
+score = int(input("What is your score? "))
+
+if score > 89:
+    print("You got an A!")
+elif score > 79 < 90:
+    print("You got a B!")
+elif score > 69 < 80:
+    print("You got a C!")
+elif score > 59 < 70:
+    print("You got a D!")
+else:
+    print("You got a F!")
