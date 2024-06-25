6ca966e70ebf492545ae399d5ca6d0b4a1f59d82
chario502 <168148447+chario502@users.noreply.github.com>
2024-06-24 20:05:15 -0400

    Create SqftRoom.py
    quick sqft calculator for a room using a function

---
+++ SqftRoom.py
@@ -0,0 +1,9 @@
+# Function Challenge
+
+def roomSize(l, w):
+    sqft = print(f"Your room is {l*w} square ft.")
+
+length = int(input("Please enter the length of the room in feet: "))
+width = int(input("Please enter the width of the room in feet: "))
+
+roomSize(length, width)
